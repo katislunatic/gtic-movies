5a41876fed8edccef5155c821ea5100eabc98bfc
katislunatic <jcharlton2220@gmail.com>
2026-01-04 20:05:50 -0600

    Update tlou.js

--- tlou.js
+++ tlou.js
@@ -21,13 +21,5 @@ const movies = [
       }
     ],
     img: "https://image-of-tlou-poster.jpg"
-  },
-  {
-    title: "Zootopia 2",
-    rating: "PG",
-    category: "Family",
-    desc: "Judy and Nick return to crack a wild mystery.",
-    link: "https://vidnest.fun/movie/1084242",
-    img: "https://disney.images.edge.bamgrid.com/ripcut-delivery/v2/variant/disney/44d994ac-3f6c-4d9d-8000-fd8f4bfc9cfc/compose?aspectRatio=1.78&format=webp&width=1200"
   }
 ];
